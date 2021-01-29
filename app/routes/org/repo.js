8a8e1356a727da6c586d1d5bdb888655d37064c6
crispinamuriel <crispinamuriel@gmail.com>
2021-01-29 16:50:42 -0500

    orgs is an array of objects

--- app/routes/org/repo.js
+++ app/routes/org/repo.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class OrgRepoRoute extends Route {
   beforeModel(/* transition */) {
-    this.replaceWith('org.repo');
+    this.replaceWith('org.repo.issues');
   }
   model(params) {
     let { repo_name } = params;
--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -2,7 +2,15 @@ import Route from '@ember/routing/route';
 
 export default class OrgsRoute extends Route {
   model() {
-    const githubOrgs = ['facebook', 'netflix', 'yahoo', 'emberjs'];
-    return githubOrgs
+    const githubOrgs = [
+      { id: "emberjs" },
+      { id: "ember-cli" },
+      { id: "microsoft" },
+      { id: "crispinamuriel" },
+      { id: "facebook" },
+      { id: "netflix" },
+      { id: "yahoo" },
+    ];
+    return githubOrgs;
   }
 }
