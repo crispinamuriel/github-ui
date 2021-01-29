fe2b438dbe091dd49ce81ba7ed447dcd41979caa
crispinamuriel <crispinamuriel@gmail.com>
2021-01-29 17:10:46 -0500

    make repo data an array of objects

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -2,7 +2,13 @@ import Route from '@ember/routing/route';
 
 export default class OrgReposRoute extends Route {
   model() {
-    const repos = ['react', 'relay', 'watchman', 'react-native', 'ember.js'];
+    const repos = [
+      { id: "react" },
+      { id: "relay" },
+      { id: "watchman" },
+      { id: "react-native" },
+      { id: "ember.js" },
+    ];
     return repos;
   }
 }
