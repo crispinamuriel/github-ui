6ab122480955e5c82032a8e21ea6d8ff3c5c53ec
crispinamuriel <crispinamuriel@gmail.com>
2021-02-03 15:55:52 -0500

    add orgs

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -3,13 +3,22 @@ import Route from '@ember/routing/route';
 export default class OrgsRoute extends Route {
   model() {
     const githubOrgs = [
+      { id: "adobe" },
+      { id: "RedHatOfficial" },
+      { id: "cfpb" },
+      { id: "netflix" },
+      { id: "esri" },
+      { id: "square" },
+      { id: "twitter" },
+      { id: "ibm" },
+      { id: "alibaba" },
       { id: "emberjs" },
       { id: "ember-cli" },
       { id: "microsoft" },
-      { id: "crispinamuriel" },
+      { id: "godaddy" },
       { id: "facebook" },
-      { id: "netflix" },
       { id: "yahoo" },
+      { id: "yelp" },
     ];
     return githubOrgs;
   }
