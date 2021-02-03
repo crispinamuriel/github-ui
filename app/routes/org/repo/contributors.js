97d58c3b2ee89896f969899008886775ba50f721
crispinamuriel <crispinamuriel@gmail.com>
2021-02-03 15:47:58 -0500

    exercise 4 solution

--- app/routes/org/repo/contributors.js
+++ app/routes/org/repo/contributors.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default class OrgRepoContributorsRoute extends Route {
+  async model() {
+
+    const org_name = this.modelFor('org');
+    const repo_name = this.modelFor('org.repo');
+
+    const data = await fetch(`https://api.github.com/repos/${org_name}/${repo_name}/contributors`)
+        .then((response) => response.json())
+        .then((json) => {
+          return json;
+        }).catch((err) => console.log('Request Error: ', err));
+
+    return data
+  }
 }
--- app/routes/org/repo/issues.js
+++ app/routes/org/repo/issues.js
@@ -1,4 +1,28 @@
 import Route from '@ember/routing/route';
 
 export default class OrgRepoIssuesRoute extends Route {
+  async model() {
+    let org_name = this.modelFor('org');
+    let repo_name = this.modelFor('org.repo');
+
+    const data = await fetch(`https://api.github.com/repos/${org_name}/${repo_name}/issues`)
+        .then((response) => response.json())
+        .then((json) => {
+          return json;
+        }).catch((err) => console.log("Request Failed: ", err));
+
+    return data;
+  }
 }
+// async model() {
+//     let org_name = this.modelFor('org');
+
+//     const data = await fetch(`https://api.github.com/orgs/${org_name}/repos`)
+//       // Handle success
+//       .then((response) => response.json()) // convert to json
+//       .then((json) => {
+//         return json; // return json to data variable
+//       })
+//       .catch((err) => console.log("Request Failed", err)); // catch errors
+//     return data;
+//   }
