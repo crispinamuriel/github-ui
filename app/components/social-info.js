b91e19e364398ca9a0daa1b48e9fd3a0e2ca0e74
crispinamuriel <crispinamuriel@gmail.com>
2021-02-08 20:24:51 -0500

    passing props to template/components

---
+++ app/components/social-info.js
@@ -0,0 +1,24 @@
+import Component from "@ember/component";
+export default Component.extend({
+
+});
+// import Ember from 'ember';
+// export default Ember.Component.extend({
+
+// });
+
+/* https://medium.com/@Dhaulagiri/embers-javascript-modules-api-b4483782f329
+Examples of incorrect code for this rule:
+
+Ember.Component.extend({});
+Ember.Object.extend({});
+Ember.computed(function () {});
+Ember.inject.service('foo');
+
+
+Examples of correct code for this rule:
+
+import Component from '@ember/component';
+import EmberObject, { computed } from '@ember/object';
+import Service, { inject } from '@ember/service';
+*/
