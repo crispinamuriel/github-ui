38731c535e5af69f3a67ecf6311342b9af4cd350
crispinamuriel <crispinamuriel@gmail.com>
2021-02-14 20:57:32 -0500

    ember octane diff

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -1,4 +1,12 @@
+/* eslint-disable ember/no-get */
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class NotFoundRoute extends Route {
+  @service('authentication')
+
+  setupController(controller) {
+    super.setupController(...arguments);
+        controller.set('records', this.get('authentication.records'));
+  }
 }
---
+++ app/services/authentication.js
@@ -0,0 +1,7 @@
+import Service from '@ember/service';
+
+export default class AuthenticationService extends Service {
+  getRecords() {
+    return 'hi';
+  }
+}
